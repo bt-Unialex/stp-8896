6feec89eac2095a272ec100ecebab1be06e70715
kostyaslob <kostyaslob@gmail.com>
2025-05-06 16:28:40 +0300

    update js for header and reviews

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,24 +1,24 @@
-import Swiper from 'swiper/bundle';
-import 'swiper/css/bundle';
+import Swiper from "swiper/bundle";
+import "swiper/css/bundle";
 
-const reviewsSlider = new Swiper('[data-reviews-swiper]', {
+const reviewsSlider = new Swiper("[data-reviews-swiper]", {
   slidesPerView: 1.1,
   centeredSlides: true,
   slidesPerGroup: 1,
   spaceBetween: 8,
 
   pagination: {
-    el: '[data-reviews-pagination]',
+    el: "[data-reviews-pagination]",
     clickable: true,
-    bulletClass: 'reviews-swipper-pagination',
-    bulletActiveClass: 'active',
+    bulletClass: "reviews-swipper-pagination",
+    bulletActiveClass: "active",
     renderBullet: function (index, className) {
       return `<div class="${className}" data-reviews-page="${index}"></div>`;
     },
   },
   navigation: {
-    nextEl: '[data-btn-right]',
-    prevEl: '[data-btn-left]',
+    nextEl: "[data-btn-right]",
+    prevEl: "[data-btn-left]",
   },
   keyboard: {
     enabled: true,
