a4d1271b69fbe17e25f994748b0c3d235a4bf859
Oleksiy <Unialex@ukr.net>
2025-05-07 12:53:35 +0300

    polish poppu js

--- src/js/pop-up.js
+++ src/js/pop-up.js
@@ -1,28 +1,21 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const popup = document.querySelector("[data-popup-overlay]");
-    const accept = document.querySelector("[data-popup-accept]");
-    const decline = document.querySelector("[data-popup-decline]");
-    const cookiesChosen = localStorage.getItem("cookiesChosen");
+document.addEventListener('DOMContentLoaded', () => {
+  const LOCAL_STORAGE_KEY = 'cookiesChosen';
+  const cookiesChosen = localStorage.getItem(LOCAL_STORAGE_KEY);
 
-    if (!cookiesChosen) {
-      popup.setAttribute("data-visible", "open");
-      document.body.setAttribute("data-no-scroll", "");
-    }
+  if (!cookiesChosen) {
+    const popup = document.querySelector('[data-popup-overlay]');
+    popup.setAttribute('data-visible', 'open');
+    document.body.setAttribute('data-no-scroll', '');
 
-    accept.addEventListener("click", event => {
-        event.preventDefault();
-        localStorage.setItem("cookiesChosen", "accepted");
-        closePopup();
-    });
-
-    decline.addEventListener("click", event => {
-        event.preventDefault();
-        localStorage.setItem("cookiesChosen", "declined");
-        closePopup();
-    });
-
-    function closePopup() {
-        popup.setAttribute("data-visible", "close");
-        document.body.removeAttribute("data-no-scroll");
-    }
+    const handleClick = evt => {
+      const choice = evt.target.dataset.popupBtn;
+      if (choice) {
+        localStorage.setItem(LOCAL_STORAGE_KEY, choice);
+        popup.setAttribute('data-visible', 'close');
+        document.body.removeAttribute('data-no-scroll');
+        popup.removeEventListener('click', handleClick);
+      }
+    };
+    popup.addEventListener('click', handleClick);
+  }
 });
