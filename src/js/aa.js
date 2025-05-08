b21cee91de69989a2d3b15d680384e6483d05536
Oleksiy <Unialex@ukr.net>
2025-05-08 16:42:55 +0300

    fix

--- src/js/aa.js
+++ src/js/aa.js
@@ -1,6 +1,6 @@
-import smallest from "/img/hero/aabg_smalest_d.png"
-import small from "/img/hero/aabg_smalest_d.png"
-import large from "/img/hero/aabg_smalest_d.png"
+import smallest from '/img/hero/aabg_smalest_d.png';
+import small from '/img/hero/aabg_smalest_d.png';
+import large from '/img/hero/aabg_smalest_d.png';
 
 document.addEventListener('DOMContentLoaded', () => {
   const aadiv = document.querySelector('.aadiv');
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let rule;
     if (choice) {
       switch (choice) {
-        case 'ss':url(/stp-8896/assets/hero_bg_d-85202e12.png)
+        case 'ss':
           rule = `background-image: url(${smallest});`;
           break;
         case 'sl':
